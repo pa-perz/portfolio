030c64c7e610984e503ae17d32048efc6c3ae8a6
Pablo Pérez <pablo@pa-perz.com>
2021-08-16 21:49:09 +0100

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,8 @@ class MyDocument extends Document {
 
   render() {
     const meta = {
-      title: 'Next.js Blog Starter Kit',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
+      title: 'Pablo Pérez - Software Developer',
+      description: 'I\'m still learning...',
       image:
         'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
     }
